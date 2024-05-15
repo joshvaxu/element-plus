ac4bbbc7e62079a3e6ac1bc94e026a882e41ccda
joshvaxu <joshvaxu@hotmail.com>
2024-05-15 11:54:53 +0800

    Update basic-usage.vue

--- docs/examples/select/basic-usage.vue
+++ docs/examples/select/basic-usage.vue
@@ -1,67 +1,31 @@
 <template>
-  <div class="flex flex-wrap gap-4 items-center">
-    <el-select
-      v-model="value"
-      placeholder="Select"
-      size="large"
-      style="width: 240px"
-    >
+  <div>
+    <el-select v-model="selectedOption" placeholder="请选择">
       <el-option
         v-for="item in options"
         :key="item.value"
         :label="item.label"
         :value="item.value"
-      />
-    </el-select>
-    <el-select v-model="value" placeholder="Select" style="width: 240px">
-      <el-option
-        v-for="item in options"
-        :key="item.value"
-        :label="item.label"
-        :value="item.value"
-      />
-    </el-select>
-    <el-select
-      v-model="value"
-      placeholder="Select"
-      size="small"
-      style="width: 240px"
-    >
-      <el-option
-        v-for="item in options"
-        :key="item.value"
-        :label="item.label"
-        :value="item.value"
-      />
+      >
+        <el-tooltip :content="item.tooltip" placement="right">
+          <span>{{ item.label }}</span>
+        </el-tooltip>
+      </el-option>
     </el-select>
   </div>
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue'
-
-const value = ref('')
-
-const options = [
-  {
-    value: 'Option1',
-    label: 'Option1',
-  },
-  {
-    value: 'Option2',
-    label: 'Option2',
-  },
-  {
-    value: 'Option3',
-    label: 'Option3',
-  },
-  {
-    value: 'Option4',
-    label: 'Option4',
-  },
-  {
-    value: 'Option5',
-    label: 'Option5',
-  },
-]
+<script>
+export default {
+  data() {
+    return {
+      selectedOption: null,
+      options: [
+        { value: 'option1', label: 'Option 1', tooltip: 'Tooltip for Option 1' },
+        { value: 'option2', label: 'Option 2', tooltip: 'Tooltip for Option 2' },
+        { value: 'option3', label: 'Option 3', tooltip: 'Tooltip for Option 3' }
+      ]
+    };
+  }
+};
 </script>
